50055f1e496ecfbb7dc7cf02b92999999dd94c8a
lawrencemburia8@gmail.com <lawrencemburia8@gmail.com>
2023-04-15 11:09:53 +0300

    editing js file

--- time.js
+++ time.js
@@ -1,35 +1,7 @@
-/*
-setInterval(time,1000);
-function time(){
-var d = new Date();
-  
-  
-  
-  var text ="Continue betting with us";
-var output = document.getElementById("welcome");
-var i = 0;
-function writeText(){
-  
-  output.innerHTML= text.slice(0,i);
-  i++;
-  if(i>text.length){
-    i=0;
-  }
-  
-}
-setInterval(writeText,150);
-  
-  */
+
   
   var c = document.getElementById('myCanvas');
 let ctx = c.getContext('2d');
-//drawing a line
-//ctx.moveTo(0,0);
-//ctx.lineTo(500,500);
-//ctx.stroke
-
-
-/*drawing texts*/
 
 ctx.font='18px myFont';
 ctx.fillStyle = "white";
